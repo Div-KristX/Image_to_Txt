6887adf929264ffac72fd367e56640c20dedfcd7
Vladyslav <70589107+Div-KristX@users.noreply.github.com>
2021-10-11 21:48:42 +0300

    uppdate

--- Code/Image.java
+++ Code/Image.java
@@ -7,10 +7,10 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.io.*;
 public class Image {
-    public void converter(){
+    public void converter(String path){
         try {
-            File file = new File("example.jpg");
-           // File file = new File("camo-forest.jpg");
+            System.out.println("Converting to W/B...");
+            File file = new File(path);
             BufferedImage source = ImageIO.read(file);
             BufferedImage result = new BufferedImage(source.getWidth(),source.getHeight(),source.getType());
             for(int x =0; x<source.getWidth();x++){
@@ -32,6 +32,7 @@ public class Image {
             }
             File output = new File ("WhiteBlackImage.jpg");
             ImageIO.write(result,"jpg",output);
+            System.out.println("Converted to W/B");
 
         } catch (IOException e){
 
@@ -39,18 +40,39 @@ public class Image {
         }
 
     }
-    public void TextBuilder()  {
+    public void TextBuilderCustom()  {
         try {
             File TextedFile = new File("Texted Image.txt");
             FileWriter writer = new FileWriter(TextedFile);
             File jpg = new File("WhiteBlackImage.jpg");
             BufferedImage source = ImageIO.read(jpg);
             boolean error = false;
+            boolean symbol = false;
+            String DefaultB = "*";
+            String DefaultD = "&";
             int min = 256;
             int[] ValuesColor = new int[255];
             int[] ValuesCount = new int[255];
-
+            Scanner c = new Scanner(System.in);
+            Scanner SB = new Scanner(System.in);
+            Scanner SD = new Scanner(System.in);
             int count = 0;
+            System.out.println("Change default symbols? * - for bright, & - for dark ");
+            System.out.println("Write 1 to change -");
+            int chars = c.nextInt();
+            if(chars == 1){
+                symbol=true;
+                System.out.print("Write symbol for bright color - ");
+                String newBsymb = SB.nextLine();
+                System.out.println(" ");
+                System.out.print("Write symbol for dark color - ");
+                String newDsymb = SD.nextLine();
+                DefaultB = newBsymb;
+                DefaultD = newDsymb;
+            }else {
+                symbol=false;
+                System.out.println("Symbols will not change");
+            }
             for(int y=0;y<source.getHeight();y++){
                 for(int x=0;x<source.getWidth();x++){
                     Color color = new Color(source.getRGB(x,y));
@@ -83,7 +105,7 @@ public class Image {
             System.out.print("Choose the number of a color"+" (from "+0+" to "+count+") - " );
             Scanner CN = new Scanner(System.in);
             int Number = CN.nextInt();
-            if(Number>count){
+            if(Number>count||Number <0){
                 System.out.println("Error, empty color detected");
                 error=true;
             }
@@ -109,39 +131,99 @@ public class Image {
                         int RGB = color.getBlue();
 
                          if(RGB <=ValuesColor[Number]){
-                             if (RGB<=16){
-                                 writer.write("&");
+                             if (RGB<=30){
+                                 writer.write(DefaultD);
                              }else {
-                                 writer.write("*");
+                                 writer.write(DefaultB);
 
                              }
-
-
-
                         } else {
                             writer.write(" ");
                         }
 
-
-                        /*
-                        if(RGB<=255&&RGB>=230){
-                            writer.write(" ");
-                        }
-                        else if(RGB <=ValuesColor[Number]){
-                            writer.write("&");
-                            if(RGB<=3){
-                                writer.write("&");}
-                            else {
-                                writer.write("*");
-
-                            }
-
-                        }*/
                     }
                     writer.write("\r\n");
                 }}
             writer.flush();
             writer.close();
+            System.out.println("--Success--");
+        }catch (IOException e){
+
+            System.out.println("Invalid path or Out of memory");
+        }
+
+
+    }
+    public void TextBuilderAuto()  {
+        try {
+            File TextedFile = new File("Texted Image.txt");
+            FileWriter writer = new FileWriter(TextedFile);
+            File jpg = new File("WhiteBlackImage.jpg");
+            BufferedImage source = ImageIO.read(jpg);
+            int min = 256;
+            int[] ValuesColor = new int[255];
+            int[] ValuesCount = new int[255];
+
+            int count = 0;
+            for(int y=0;y<source.getHeight();y++){
+                for(int x=0;x<source.getWidth();x++){
+                    Color color = new Color(source.getRGB(x,y));
+                    int RGB = color.getBlue();
+                    if(min>RGB){
+                        min=RGB;
+                        ValuesColor[count]=min;
+                        ++count;
+
+                    }
+                }
+            }
+            for(int y=0;y<source.getHeight();y++){
+                for(int x=0;x<source.getWidth();x++){
+                    Color color = new Color(source.getRGB(x,y));
+                    int RGB = color.getBlue();
+                    for(int k=0; k<=count;k++){
+                        if(ValuesColor[k]==RGB){
+                            ValuesCount[k]=ValuesCount[k]+1;
+                        }
+                    }
+                }
+            }
+            int Sum=0;
+        for(int i =0 ; i<=count;i++){
+
+            Sum=Sum+ValuesCount[i];
+        }
+            int Avg = Sum/count;
+            int check=0;
+        for(int i=0;i<=count;i++){
+            if(ValuesCount[i]>=Avg){
+               check=i+1;
+            } }
+
+
+                for(int y=0;y<source.getHeight();y++){
+                    for(int x=0;x<source.getWidth();x++){
+                        writer.write(" ");
+                        Color color = new Color(source.getRGB(x,y));
+                        int RGB = color.getBlue();
+
+                        if(RGB <=ValuesColor[check]){
+                            if (RGB<=31){
+                                writer.write("&");
+                            }else {
+                                writer.write("*");
+
+                            }
+                        } else {
+                            writer.write(" ");
+                        }
+
+                    }
+                    writer.write("\r\n");
+                }
+            writer.flush();
+            writer.close();
+            System.out.println("--Success--");
 
         }catch (IOException e){
 
